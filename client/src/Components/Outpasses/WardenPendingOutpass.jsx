9048792321caa557fee866cfd5664395700df8a1
arjav jain <arjavjain700@gmail.com>
2024-01-28 01:49:05 +0530

    created page to display whole details of the outpass

--- client/src/Components/Outpasses/WardenPendingOutpass.jsx
+++ client/src/Components/Outpasses/WardenPendingOutpass.jsx
@@ -16,22 +16,22 @@ const WardenPendingOutpass = () => {
             <form>
                 <p>JUET Outpass</p>
                 <label htmlFor="fname" className="item-1">Name:
-                    <input type="text" name="fname" value="Arjav Jain" className="sa-pass"/>
+                    <input type="text" name="fname" value="Arjav Jain" className="sa-pass" disabled/>
                 </label>
                 <label htmlFor="enroll"className="item-2">Enrollment No. :
-                    <input type="text" name="enroll" value="201B060" className="sa-pass sa-enroll"/>
+                    <input type="text" name="enroll" value="201B060" className="sa-pass sa-enroll" disabled/>
                 </label>
                 <label htmlFor="from"className="item-3">From:
-                    <input type="date" name="from" value="2024-02-01" className="sa-pass sa-date"/>
+                    <input type="date" name="from" value="2024-02-01" className="sa-pass sa-date" disabled/>
                 </label>
                 <label htmlFor="to" className="item-4">To:
-                    <input type="date" name="to" value="2024-02-03" className="sa-pass sa-date"/>
+                    <input type="date" name="to" value="2024-02-03" className="sa-pass sa-date" disabled/>
                 </label>
                 <label htmlFor="adress"className="item-5">Adress:
-                    <input type="text" name="adress" value="Bhopal (M.P)" className="sa-pass"/>
+                    <input type="text" name="adress" value="Bhopal (M.P)" className="sa-pass" disabled/>
                 </label>
                 <label htmlFor="purpose"className="item-6">Purpose:
-                    <input type="text" name="purpose" value="Family Function" className="sa-pass"/>
+                    <input type="text" name="purpose" value="Family Function" className="sa-pass" disabled/>
                 </label>
                 <input type="button" value="Approve" className="outpassbtn item-7 approve"/>
                 <input type="button" value="Reject" className="outpassbtn item-8 reject"/>
